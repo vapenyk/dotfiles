afb5c30aa9c8a2a55357bb87e8e0514e7d3a7301
Alexender Yatsenko <ayatsenkok@gmail.com>
2026-02-24 04:27:21 +0200

    small changes at osconfig/git.sh

--- osconfig/git.sh
+++ osconfig/git.sh
@@ -4,4 +4,4 @@ git config -f ~/.gitconfig_local user.signingkey "key::$(ssh-add -L | grep 'sign
 git config -f ~/.gitconfig_local gpg.format ssh
 git config -f ~/.gitconfig_local commit.gpgsign true
 
-echo "ayatsenkok@gmail.com $(ssh-add -L | grep 'signingkey$')" > ~/.ssh/allowed_signers
+echo "$(git config user.email) $(ssh-add -L | grep 'signingkey$')" > ~/.ssh/allowed_signers
